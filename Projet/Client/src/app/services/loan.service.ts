af57b3398e684318f94aa455eba06f6aac2255bb
RenaudC5 <Colin.renaud5@gmail.com>
2021-12-16 10:58:12 +0100

    Implémentation emprunt livre client

--- Projet/Client/src/app/services/loan.service.ts
+++ Projet/Client/src/app/services/loan.service.ts
@@ -8,8 +8,14 @@ import { BaseHttpService } from './baseHttpService';
 @Injectable()
 export class LoanService extends BaseHttpService {
   loan(copyId, userId): Observable<void> {
+
+    const date = new Date()
     /* to be changed */
-    return of({})
+    return this.http.post(`${this.baseUrl}/loans`,{
+      loanDate: `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`,
+      copyId: copyId,
+      userId: userId
+    })
       .pipe(
         map(() => null),
         catchError((err) => { console.log(err); return null; })
